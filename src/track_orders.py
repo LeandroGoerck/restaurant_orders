a0aaaaab329a7415e86a103bd063e3607bb1991d
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-09-21 01:53:44 -0300

    feat(track_orders): add continuous analisys to restaurant orders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,76 @@
+from multiset import Multiset
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        orders_by_customer = Multiset()
+        for order in self.orders:
+            person = order[0]
+            product = order[1]
+
+            if person == customer:
+                orders_by_customer.add(product)
+
+        return max(orders_by_customer.distinct_elements(),
+                   key=lambda x: orders_by_customer[x])
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        all_orders = set()
+        orders_by_customer = Multiset()
+        for order in self.orders:
+            person = order[0]
+            product = order[1]
+
+            if person == customer:
+                orders_by_customer.add(product)
+            all_orders.add(product)
+
+        return (all_orders - orders_by_customer.distinct_elements())
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        all_days = set()
+        days_by_customer = Multiset()
+        for order in self.orders:
+            person = order[0]
+            day = order[2]
+
+            if person == customer:
+                days_by_customer.add(day)
+            all_days.add(day)
+
+        return (all_days - days_by_customer.distinct_elements())
 
     def get_busiest_day(self):
-        pass
+        order_by_days = Multiset()
+        for order in self.orders:
+            day = order[2]
+
+            if day not in order_by_days:
+                order_by_days.add(day)
+
+            order_by_days.add(day)
+
+        return max(order_by_days.distinct_elements(),
+                   key=lambda x: order_by_days[x])
 
     def get_least_busy_day(self):
-        pass
+        order_by_days = Multiset()
+        for order in self.orders:
+            day = order[2]
+
+            if day not in order_by_days:
+                order_by_days.add(day)
+
+            order_by_days.add(day)
+
+        return min(order_by_days.distinct_elements(),
+                   key=lambda x: order_by_days[x])
